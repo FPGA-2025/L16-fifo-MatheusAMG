b7ced7fa68b759854b89c31fa887cc7bf0c50c4b
Matheus <matheusamg@gmail.com>
2025-05-13 10:39:18 -0300

    tentativa com ciclos diferentes

--- fifo.v
+++ fifo.v
@@ -9,7 +9,34 @@ module fifo(
     output empty
 );
 
-// insira seu código aqui
+reg [7:0] fila_fifo [3:0]; //declarar 4 registradores de 8 bits
+reg [1:0] pt_wr; //ponteiro de escrita
+reg [1:0] pt_rd; //ponteiro de leitura
+reg [4:0] counter;
+
+assign full  = (counter == 4);
+assign empty = (counter == 0);
+
+// pode entrar e sair dados ao mesmo tempo
+always @(posedge clk, negedge rstn) begin
+    if(~rstn) begin
+        pt_wr <=0;
+        pt_rd <=0;
+        counter <= 0;
+    end
+    else begin
+        if(wr_en && ~full)begin
+            fila_fifo[pt_wr] <= data_in;
+            pt_wr <= (pt_wr == 3) ? 0 : pt_wr + 1;
+            counter <= counter + 1;
+        end
+        if(rd_en && ~empty)begin
+            data_out <= fila_fifo[pt_rd];
+            pt_rd <= (pt_rd == 3) ? 0 : pt_rd + 1;
+            counter <= counter - 1;
+        end
+    end
+end
 
 endmodule
 
